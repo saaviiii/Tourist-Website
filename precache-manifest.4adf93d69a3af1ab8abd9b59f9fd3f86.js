8cb83f6b81142ed83535bfea32281a6df999a36c
saaviiii <agarwalsaavi28@gmail.com>
2023-07-25 17:08:44 +0530

    Updates

---
+++ precache-manifest.4adf93d69a3af1ab8abd9b59f9fd3f86.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = [
+  {
+    "revision": "a6c22cd3e450d881e51e",
+    "url": "/Tourist-Website/static/css/main.022119cd.chunk.css"
+  },
+  {
+    "revision": "a6c22cd3e450d881e51e",
+    "url": "/Tourist-Website/static/js/main.5a1ecad6.chunk.js"
+  },
+  {
+    "revision": "3ac1b5dda8aaff0ce9a6",
+    "url": "/Tourist-Website/static/js/runtime~main.3ed6a851.js"
+  },
+  {
+    "revision": "ca88bef8e5d52c25275c",
+    "url": "/Tourist-Website/static/css/2.fc4ba877.chunk.css"
+  },
+  {
+    "revision": "ca88bef8e5d52c25275c",
+    "url": "/Tourist-Website/static/js/2.4328dd5f.chunk.js"
+  },
+  {
+    "revision": "1187699ecd58d62ab7fdf5c42d891e85",
+    "url": "/Tourist-Website/static/media/bg2.1187699e.jpg"
+  },
+  {
+    "revision": "dbee29a7e78f277f5b5d305d15cc1ae6",
+    "url": "/Tourist-Website/static/media/bg.dbee29a7.png"
+  },
+  {
+    "revision": "6af1f940febfb470e6b4e3fd0bc8fa6a",
+    "url": "/Tourist-Website/static/media/img-card (1).6af1f940.jpg"
+  },
+  {
+    "revision": "8de4852b189f9dd674e031fbead6e5bb",
+    "url": "/Tourist-Website/static/media/img-card (2).8de4852b.jpg"
+  },
+  {
+    "revision": "49b0a112ffbe37f707a1a785a24d46eb",
+    "url": "/Tourist-Website/static/media/img-card (3).49b0a112.jpg"
+  },
+  {
+    "revision": "3c00d79b8a4e54b7b782e94e06f73091",
+    "url": "/Tourist-Website/static/media/img-card (4).3c00d79b.jpg"
+  },
+  {
+    "revision": "7867380651433da03e1bd21bc36f5ec3",
+    "url": "/Tourist-Website/static/media/beach.78673806.jpg"
+  },
+  {
+    "revision": "79fdb461c738174a7201402a82089437",
+    "url": "/Tourist-Website/static/media/resort.79fdb461.jpg"
+  },
+  {
+    "revision": "26206feae9e35179141a2e6c25188270",
+    "url": "/Tourist-Website/static/media/camp.26206fea.jpg"
+  },
+  {
+    "revision": "febae19d6432cfd722a10e104c0a68cd",
+    "url": "/Tourist-Website/static/media/mountain.febae19d.jpg"
+  },
+  {
+    "revision": "3d63c9d4836635c380210080c579c7e8",
+    "url": "/Tourist-Website/static/media/gulp.3d63c9d4.jpg"
+  },
+  {
+    "revision": "74a3598fb7f369e371306a587d1fb0c6",
+    "url": "/Tourist-Website/static/media/img-card (5).74a3598f.jpg"
+  },
+  {
+    "revision": "362b882f8109d244f853993729764df4",
+    "url": "/Tourist-Website/static/media/img-card (6).362b882f.jpg"
+  },
+  {
+    "revision": "9cbb33d11f2f21d024318d3eb42f0489",
+    "url": "/Tourist-Website/static/media/img-card (7).9cbb33d1.jpg"
+  },
+  {
+    "revision": "961d14575cd5e2d595b1e2d2e65f99f8",
+    "url": "/Tourist-Website/static/media/img-card (8).961d1457.jpg"
+  },
+  {
+    "revision": "22d409d8b5686b39fe598665e8005654",
+    "url": "/Tourist-Website/static/media/img-card (9).22d409d8.jpg"
+  },
+  {
+    "revision": "b79f100ee8d63006320a68ea0f75222d",
+    "url": "/Tourist-Website/static/media/barobaybay.b79f100e.jpg"
+  },
+  {
+    "revision": "0cf9a246aebd95c3768bfdbf58e8bf95",
+    "url": "/Tourist-Website/static/media/award.0cf9a246.svg"
+  },
+  {
+    "revision": "171ba61fc43e1aa5851c232315001107",
+    "url": "/Tourist-Website/static/media/shield.171ba61f.svg"
+  },
+  {
+    "revision": "1d4ab0808ed85d8b7bf52b4a7acb9b11",
+    "url": "/Tourist-Website/static/media/compass.1d4ab080.svg"
+  },
+  {
+    "revision": "502189aef3b96acbcddedbb54a043638",
+    "url": "/Tourist-Website/static/media/heart.502189ae.svg"
+  },
+  {
+    "revision": "4e2c0a1d4a1adb1a99a69310e06fbbc7",
+    "url": "/Tourist-Website/static/media/trending-up.4e2c0a1d.svg"
+  },
+  {
+    "revision": "e82605c7d2dae238c36c9a96e3b46d03",
+    "url": "/Tourist-Website/static/media/flag.e82605c7.svg"
+  },
+  {
+    "revision": "0f1222c0565a6b56d03c740e23803859",
+    "url": "/Tourist-Website/static/media/monitor.0f1222c0.svg"
+  },
+  {
+    "revision": "b2f804bda31dee415fc2b1ff7ba9315f",
+    "url": "/Tourist-Website/static/media/map-pin.b2f804bd.svg"
+  },
+  {
+    "revision": "2b23e08ac21b7ce2aa82dd856d314841",
+    "url": "/Tourist-Website/static/media/phone.2b23e08a.svg"
+  },
+  {
+    "revision": "20e8982ec7e03638afe27f5c3e897681",
+    "url": "/Tourist-Website/static/media/mail.20e8982e.svg"
+  },
+  {
+    "revision": "180a6ca60002b6b9a88b1977232f25fb",
+    "url": "/Tourist-Website/static/media/facebook.180a6ca6.svg"
+  },
+  {
+    "revision": "43449f1c48de00f0ef903fcacf487626",
+    "url": "/Tourist-Website/static/media/twitter.43449f1c.svg"
+  },
+  {
+    "revision": "beb7135630c8d3725bc3571be62ad56a",
+    "url": "/Tourist-Website/static/media/linkedin.beb71356.svg"
+  },
+  {
+    "revision": "6cbcb76e85761740a85b040d708c26bb",
+    "url": "/Tourist-Website/static/media/instagram.6cbcb76e.svg"
+  },
+  {
+    "revision": "e946269206724b7e3d186946b5cf711a",
+    "url": "/Tourist-Website/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/Tourist-Website/precache-manifest.4adf93d69a3af1ab8abd9b59f9fd3f86.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/Tourist-Website/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
